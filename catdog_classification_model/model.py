53380ab30359d4cca0144b9187733a95a061abb5
pwochner <pwochner@turing.ac.uk>
2022-09-26 16:01:10 +0100

    Add todos for model.py

--- catdog_classification_model/model.py
+++ catdog_classification_model/model.py
@@ -10,6 +10,12 @@ CLASS_LABELS = ["cat", "dog"]
 
 class CatDogClassifier:
     def __init__(self):
+        ### TODO ###
+        ### Download the file containing the weights of the pre-trained model from url
+        ### if the file doesn't exist already locally, and write it to a file.
+        ### After that, create an instance of the model class.
+        ### The code to load the model weights from file and evaluate the model is
+        ### already provided.
         filename = "conv_net_model3.ckpt"
         if not os.path.exists(filename):
             model_path = os.path.join(
@@ -22,27 +28,23 @@ class CatDogClassifier:
         self.model.load_state_dict(torch.load(filename))
         self.model.eval()
 
-    def predict(self, input):
-        # transform input image (as required by model)
-        transform_input = transforms.Compose(
-            [
-                transforms.Resize((256, 256)),
-                transforms.Normalize(
-                    mean=[82.18, 139.30, 140.27], std=[49.40, 35.24, 37.86],
-                ),
-            ]
-        )
-        input = input.values
-        input = input[:, :, 0:3]  # make sure we have only 3 channels
-        input = np.transpose(input, (2, 0, 1))
-        input = torch.from_numpy(input).type(torch.float32)
-        input = input.unsqueeze(0)
-        input = transform_input(input)
+    def predict(self, image):
+        # pre-process input image (as required by model)
+        transform_input = transforms.Compose([transforms.Resize((256, 256)),])
+        image = image.values
+        image = image[:, :, 0:3]  # make sure we have only 3 channels
+        image = np.transpose(image, (2, 0, 1))
+        image = torch.from_numpy(image).type(torch.float32)
+        image = image.unsqueeze(0)
+        image = transform_input(image)
 
         # make prediction
-        prediction = self.model(input)  # TODO: is it model.predict() for tf?
-        prediction = prediction.detach().numpy()
-        max_val = np.max(prediction)
-        max_ind = np.argmax(prediction)
-        return f"{CLASS_LABELS[max_ind]}"
+        ### TODO ###
+        ### Apply the model to the input image.
+        ### Find the class with a higher output, and, using the list of
+        ### CLASS_LABELS, get the corresponding class name.
+        ### Bonus: If you also want the predicted probability for each class,
+        ### convert the logits into a probability.
+
+        return "FIX ME"
 
